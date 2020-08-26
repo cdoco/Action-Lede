3ad32584b477f032ac9f4f9aa4a26aeca2af006b
ZiHang Gao <gao_zihang@hotmail.com>
2020-08-26 15:48:08 +0800

    🍭

--- user/lean-complete-x64/custom.sh
+++ user/lean-complete-x64/custom.sh
@@ -2,15 +2,6 @@
 
 echo "Test custom.sh"
 
-source ../version
-sed -i '92d'                                                                    package/system/opkg/Makefile
-sed -i '/shadow/d'                                                              package/lean/default-settings/files/zzz-default-settings
-sed -i '/nas/d'                                                                 package/lean/default-settings/files/zzz-default-settings
-sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"                         package/lean/default-settings/files/zzz-default-settings
-sed -i "s/https:/R20.0.0/g"                                                     package/lean/default-settings/files/zzz-default-settings
-sed -i  's/http:/snapshots/g'                                                   package/lean/default-settings/files/zzz-default-settings
-sed -i  " 23i sed -i 's/http:/https:/g' /etc/opkg/distfeeds.conf"               package/lean/default-settings/files/zzz-default-settings
-sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g"                           package/lean/default-settings/files/zzz-default-settings
 sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'                                package/lean/autosamba/Makefile
 
 sed -i "2i # network config"                                                    package/lean/default-settings/files/zzz-default-settings
