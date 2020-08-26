fbf1d1d3728551ed3767fc720946453377091384
ZiHang Gao <gao_zihang@hotmail.com>
2020-08-26 17:58:41 +0800

    🍭

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -2,6 +2,17 @@
 
 echo "Apply custom.sh"
 
+sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'                                package/lean/autosamba/Makefile
+
+sed -i "2i # network config"                                                    package/lean/default-settings/files/zzz-default-settings
+sed -i "3i uci set network.lan.ipaddr='192.168.2.1'"                            package/lean/default-settings/files/zzz-default-settings
+sed -i "4i uci set network.lan.proto='static'"                                  package/lean/default-settings/files/zzz-default-settings
+sed -i "5i uci set network.lan.type='bridge'"                                   package/lean/default-settings/files/zzz-default-settings
+sed -i "6i uci set network.lan.ifname='eth0'"                                   package/lean/default-settings/files/zzz-default-settings
+sed -i "7i uci set network.lan.netmask='255.255.255.0'"                         package/lean/default-settings/files/zzz-default-settings
+sed -i "10i uci commit network\n"                                               package/lean/default-settings/files/zzz-default-settings
+
+rm -rf package/lean/luci-theme-argon
 git clone https://github.com/CCnut/feed-netkeeper.git -b openwrt-18.06                                   package/nk
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git                             package/luci-theme-infinityfreedom
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git                                        package/luci-theme-atmaterial
@@ -9,3 +20,6 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06            
 git clone https://github.com/tty228/luci-app-serverchan.git                                              package/luci-app-serverchan
 git clone https://github.com/vernesong/OpenClash.git -b master                                           package/luci-app-openclash
 git clone https://github.com/max0y/luci-app-cd8021x.git -b master                                        package/luci-app-cd8021x
+git clone https://github.com/destan19/OpenAppFilter.git                                                  package/oaf
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome                          package/adg
+svn co https://github.com/Lienol/openwrt-luci/trunk/applications/luci-app-smartdns                       package/luci-app-smartdns
--- user/lean-complete-x64/custom.sh
+++ user/lean-complete-x64/custom.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
 echo "Test custom.sh"
-
-sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'                                package/lean/autosamba/Makefile
-
-sed -i "2i # network config"                                                    package/lean/default-settings/files/zzz-default-settings
-sed -i "3i uci set network.lan.ipaddr='192.168.2.1'"                            package/lean/default-settings/files/zzz-default-settings
-sed -i "4i uci set network.lan.proto='static'"                                  package/lean/default-settings/files/zzz-default-settings
-sed -i "5i uci set network.lan.type='bridge'"                                   package/lean/default-settings/files/zzz-default-settings
-sed -i "6i uci set network.lan.ifname='eth0'"                                   package/lean/default-settings/files/zzz-default-settings
-sed -i "7i uci set network.lan.netmask='255.255.255.0'"                         package/lean/default-settings/files/zzz-default-settings
-sed -i "10i uci commit network\n"                                               package/lean/default-settings/files/zzz-default-settings
-
-rm -rf package/lean/luci-theme-argon
-git clone https://github.com/destan19/OpenAppFilter.git                                                  package/oaf
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome                          package/adg
-svn co https://github.com/Lienol/openwrt-luci/trunk/applications/luci-app-smartdns                       package/luci-app-smartdns
